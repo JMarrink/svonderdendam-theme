771f72cdb8ac8feca99f1597c1b2643f469969b7
JMarrink <jensmarrink@gmail.com>
2025-06-03 22:46:11 +0200

    removed old code

--- templates/parts/latest_news.php
+++ templates/parts/latest_news.php
@@ -5,65 +5,35 @@
             </div>
             <div class="swiper latest-news-slider">
                 <div class="swiper-wrapper">
-            <?php
-            $args = array(
-                'category_name' => 'nieuws',
-                'posts_per_page' => 6,
-            );
-            $latest_news = new WP_Query($args);
-            if ($latest_news->have_posts()) :
-                while ($latest_news->have_posts()) : $latest_news->the_post();
-            ?>
-                    <div class="swiper-slide latest-news-card">
-                        <a href="<?php the_permalink(); ?>">
-                        <div class="latest-news-card__image">
-                            <?php if (has_post_thumbnail()) : ?>          
-                                <?php the_post_thumbnail('medium', ['alt' => get_the_title()]); ?>
-                            <?php else : ?>
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/onderdendam-highfive-milan.png" alt="Nieuws">    <?php endif; ?>
-                        </div>
-                        <div class="latest-news-card__content">
-                            <h3 class="latest-news-card__title"><?php the_title(); ?></h3>
-                        </div>
-                        </a>
-                    </div>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-            else :
-            ?>
-                <p class="latest-news__no-posts">Er zijn momenteel geen nieuwsitems beschikbaar.</p>
-            <?php endif; ?>
-                    <!-- <div class="swiper-slide latest-news-card">
-                        <a href="">
-                        <div class="latest-news-card__image">
-                            <img src="http://svo.test/wp-content/uploads/2025/05/onderdendam-highfive-milan.png" alt="Nieuws">
-                        </div>
-                        <div class="latest-news-card__content">
-                            <h3 class="latest-news-card__title">Nieuw seizoen, nieuwe kansen!</h3>
-                        </div>
-                        </a>
-                    </div>
-                    <div class="swiper-slide latest-news-card">
-                        <a href="">
-                        <div class="latest-news-card__image">
-                            <img src="http://svo.test/wp-content/uploads/2025/05/onderdendam-highfive-milan.png" alt="Nieuws">
-                        </div>
-                        <div class="latest-news-card__content">
-                            <h3 class="latest-news-card__title">Nieuw seizoen, nieuwe kansen!</h3>
-                        </div>
-                        </a>
-                    </div>
-                    <div class="swiper-slide latest-news-card">
-                        <a href="">
-                        <div class="latest-news-card__image">
-                            <img src="http://svo.test/wp-content/uploads/2025/05/onderdendam-highfive-milan.png" alt="Nieuws">
-                        </div>
-                        <div class="latest-news-card__content">
-                            <h3 class="latest-news-card__title">Nieuw seizoen, nieuwe kansen!</h3>
-                        </div>
-                        </a>
-                    </div> -->
+                    <?php
+                    $args = array(
+                        'category_name' => 'nieuws',
+                        'posts_per_page' => 6,
+                    );
+                    $latest_news = new WP_Query($args);
+                    if ($latest_news->have_posts()) :
+                        while ($latest_news->have_posts()) : $latest_news->the_post();
+                    ?>
+                            <div class="swiper-slide latest-news-card">
+                                <a href="<?php the_permalink(); ?>">
+                                <div class="latest-news-card__image">
+                                    <?php if (has_post_thumbnail()) : ?>          
+                                        <?php the_post_thumbnail('medium', ['alt' => get_the_title()]); ?>
+                                    <?php else : ?>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/onderdendam-highfive-milan.png" alt="Nieuws">    <?php endif; ?>
+                                </div>
+                                <div class="latest-news-card__content">
+                                    <h3 class="latest-news-card__title"><?php the_title(); ?></h3>
+                                </div>
+                                </a>
+                            </div>
+                    <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    else :
+                    ?>
+                        <p class="latest-news__no-posts">Er zijn momenteel geen nieuwsitems beschikbaar.</p>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="latest-news__footer">
